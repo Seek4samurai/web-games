40caefe546f019a383e3c11540f0ded3de3d8c23
Seek4samurai <grvrawat999@gmail.com>
2021-06-18 19:14:22 +0530

    added main.js and bird.js

--- flappy-bird/main.js
+++ flappy-bird/main.js
@@ -1,1 +1,30 @@
-const canvas = document.getElementById('canvas1');
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 600;
+canvas.height = 400;
+
+let spacePresssed = false;
+let angle = 0;
+let hue = 0;
+let frame = 0;
+let score = 0;
+let gamespeed = 2;
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //ctx.fillRect(10, canvas.height - 90, 50, 50);
+  bird.update();
+  bird.draw();
+  requestAnimationFrame(animate);
+}
+
+animate();
+
+window.addEventListener("keydown", function (e) {
+  if (e.code === "Space") spacePresssed = true;
+});
+
+window.addEventListener("keyup", function (e) {
+  if (e.code === "Space") spacePresssed = false;
+});
