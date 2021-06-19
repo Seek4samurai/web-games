1c39d4e42921b39038271e9fb84d84dd744c473c
Seek4samurai <grvrawat999@gmail.com>
2021-06-19 15:40:27 +0530

    added Obstacles and Collision Detection

--- flappy-bird/main.js
+++ flappy-bird/main.js
@@ -8,17 +8,20 @@ let angle = 0;
 let hue = 0;
 let frame = 0;
 let score = 0;
-let gamespeed = 2;
+let gamespeed = 4;
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   //ctx.fillRect(10, canvas.height - 90, 50, 50);
+  handleObstacles();
   bird.update();
   bird.draw();
   handleParticles();
+  handleCollisions();
   requestAnimationFrame(animate);
   angle += 0.12;
-  hue++;
+  hue += 10;
+  frame++;
 }
 
 animate();
@@ -30,3 +33,20 @@ window.addEventListener("keydown", function (e) {
 window.addEventListener("keyup", function (e) {
   if (e.code === "Space") spacePresssed = false;
 });
+
+const bang = new Image();
+bang.src = "bang.png";
+function handleCollisions() {
+  for (let i = 0; i < obstaclesArray.length; i++) {
+    if (
+      bird.x < obstaclesArray[i].x + obstaclesArray[i].width &&
+      bird.x + bird.width > obstaclesArray[i].x &&
+      ((bird.y < 0 + obstaclesArray[i].top && bird.y + bird.height > 0) ||
+        (bird.y > canvas.height - obstaclesArray[i].bottom &&
+          bird.y + bird.height < canvas.height))
+    ) {
+      ctx.drawImage(bang, bird.x, bird.y, 50, 50);
+      return true;
+    }
+  }
+}
