c38a635e5da44dec6adc93f619544ed1c70bec5e
Seek4samurai <grvrawat999@gmail.com>
2021-06-24 12:16:11 +0530

    added scoring and started fish images

--- fishbait/script.js
+++ fishbait/script.js
@@ -64,12 +64,74 @@ class Player {
 const player = new Player();
 
 // Bubbles
+const bubblesArray = [];
+class Bubble {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = canvas.height + 100;
+    this.radius = 50;
+    this.speed = Math.random() * 5 + 1;
+    this.distance;
+    this.counted = false;
+    this.sound = Math.random() <= 0.5 ? "sound1" : "sound2";
+  }
+  update() {
+    this.y -= this.speed;
+    const dx = this.x - player.x;
+    const dy = this.y - player.y;
+    this.distance = Math.sqrt(dx * dx + dy * dy);
+  }
+  draw() {
+    ctx.fillStyle = "blue";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+    ctx.stroke();
+  }
+}
+
+const bubblePop1 = document.createElement("audio");
+bubblePop1.src = "Plop.ogg";
+const bubblePop2 = document.createElement("audio");
+bubblePop2.src = "bubbles-single1.wav";
+
+function handleBubbles() {
+  if (gameFrame % 50 == 0) {
+    bubblesArray.push(new Bubble());
+  }
+  for (let i = 0; i < bubblesArray.length; i++) {
+    bubblesArray[i].update();
+    bubblesArray[i].draw();
+  }
+  for (let i = 0; i < bubblesArray.length; i++) {
+    if (bubblesArray[i].y < 0 - bubblesArray[i].radius * 2) {
+      bubblesArray.splice(i, 1);
+    }
+    if (bubblesArray[i].distance < bubblesArray[i].radius + player.radius) {
+      if (!bubblesArray[i].counted) {
+        if (bubblesArray[i].sound == "sound1") {
+          bubblePop1.play();
+        } else {
+          bubblePop2.play();
+        }
+        score++;
+        bubblesArray[i].counted = true;
+        bubblesArray.splice(i, 1);
+      }
+    }
+  }
+}
 
 // Animation Loop
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handleBubbles();
   player.update();
   player.draw();
+  ctx.fillStyle = "black";
+  ctx.fillText("Score : " + score, 10, 50);
+  gameFrame++;
   requestAnimationFrame(animate);
 }
 animate();
