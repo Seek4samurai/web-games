c95ea494be0a2d4461e90b8ec29d378c06683ab1
Seek4samurai <grvrawat999@gmail.com>
2021-06-24 13:00:04 +0530

    direction player facing

--- fishbait/script.js
+++ fishbait/script.js
@@ -23,21 +23,27 @@ canvas.addEventListener("mouseup", function () {
   mouse.click = false;
 });
 // Player
+const playerLeft = new Image();
+playerLeft.src = "fish_swim_left.png";
+const playerRight = new Image();
+playerRight.src = "fish_swim_right.png";
 class Player {
   constructor() {
     this.x = canvas.width;
     this.y = canvas.height / 2;
     this.radius = 50;
     this.angle = 0;
-    this.framex = 0;
-    this.framey = 0;
+    this.frameX = 0;
+    this.frameY = 0;
     this.frame = 0;
     this.spriteWidth = 498;
-    this.spriteheight = 327;
+    this.spriteHeight = 327;
   }
   update() {
     const dx = this.x - mouse.x;
     const dy = this.y - mouse.y;
+    let theta = Math.atan2(dy, dx);
+    this.angle = theta;
     if (mouse.x != this.x) {
       this.x -= dx / 20;
     }
@@ -59,6 +65,36 @@ class Player {
     ctx.fill();
     ctx.closePath();
     ctx.fillRect(this.x, this.y, this.radius, 10);
+
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
+    if (this.x >= mouse.x) {
+      ctx.drawImage(
+        playerLeft,
+        this.frameX * this.spriteWidth,
+        this.frameY * this.spriteHeight,
+        this.spriteWidth,
+        this.spriteHeight,
+        0 - 60,
+        0 - 45,
+        this.spriteWidth / 4,
+        this.spriteHeight / 4
+      );
+    } else {
+      ctx.drawImage(
+        playerRight,
+        this.frameX * this.spriteWidth,
+        this.frameY * this.spriteHeight,
+        this.spriteWidth,
+        this.spriteHeight,
+        0 - 60,
+        0 - 45,
+        this.spriteWidth / 4,
+        this.spriteHeight / 4
+      );
+    }
+    ctx.restore();
   }
 }
 const player = new Player();
